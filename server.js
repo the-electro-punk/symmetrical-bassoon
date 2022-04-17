61a9554b5185f754e8ff30fa4b1230aacec8a4a5
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-04-17 15:42:58 -0500

    updated server

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ mongoose.connect(MONGODB_URI, {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
